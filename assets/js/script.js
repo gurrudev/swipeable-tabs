6af331beee43a2852461798a677fd028c5d7a2e6
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-05-30 13:13:05 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+
+
+let swiper = new Swiper('.swiper-container', {
+	pagination: '.swiper-pagination',
+	slidesPerView: 1,
+	paginationClickable: true,
+	loop: true,
+    paginationBulletRender: function (index, className) {
+		let tabsName = ['All', 'Credit Only', 'Debit Only',"ACD"];
+
+		if(tabsName[0] == "All"){
+			let data1 = document.getElementById('data1');
+			data1.textContent= "Hello"
+		} 
+		if (tabsName[1] == "Credit Only"){
+			let data2 = document.getElementById('data2');
+			data2.textContent= "Hi"
+		}
+
+		if (tabsName[2] == "Debit Only"){
+			let data3 = document.getElementById('data3');
+			data3.textContent= "Bye"
+			// console.log(data3)
+		}
+
+		if ( index === (tabsName.length - 1) ) {
+          	return	'<span class="' + className + '">'
+          		+ tabsName[index] + '</span>'
+          		+ '<div class="active-mark "></div>';
+		}
+		return '<span class="' + className + '">' + tabsName[index] + '</span>';
+		
+    }
+});
+
